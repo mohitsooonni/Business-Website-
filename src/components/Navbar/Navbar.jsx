f2968d98c74d1fb0bd2b0eb5216e6fe1644b8a15
Mohitsooonni <Mohitsooonni@gmail.com>
2023-09-22 18:56:12 +0530

    first Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="container">
+        <div className="n-container">
+          <div className="n-logo">
+            <span>Growbizz</span>
+          </div>
+
+          <div className="n-right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
